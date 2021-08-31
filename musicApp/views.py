8a41a742d451b589d2023644f9d829ff27452363
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2021-08-31 23:37:47 +0100

    fixted Pagination error

--- musicApp/views.py
+++ musicApp/views.py
@@ -6,7 +6,8 @@ from .models import Song
 # Create your views here.
 
 def index(request):
-    paginator = Paginator(Song.objects.all(), 1)
+    song_profile = Song.objects.get_queryset().order_by('id')
+    paginator = Paginator(song_profile, 1)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {"page_obj": page_obj}
